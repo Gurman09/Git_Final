3ebf64c097aa18d695c429a361015812c3b0e2e2
Anvi <157106785+AnviSingroha@users.noreply.github.com>
2024-05-19 20:39:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const keys = document.querySelectorAll('.key');
 
 keys.forEach(key => {
 
-    key.addEventListener('click', () => {
         // Here you can define what happens when a key is clicked
         console.log(`Clicked ${key.textContent}`);
     });
